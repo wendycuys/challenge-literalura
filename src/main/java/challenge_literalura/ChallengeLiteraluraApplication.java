3894398922d1526a8038a6a4c200c6b6b2190aae
wendycuys <wendycuy@192.168.0.4>
2025-01-19 13:04:12 -0500

    Create proyect

---
+++ src/main/java/challenge_literalura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package challenge_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+}
